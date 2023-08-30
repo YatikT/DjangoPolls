d4037ef96c5129ae9fb6daad3a7499c2a9ce1aad
Yatik Thakwani <yatikthakwani@gmail.com>
2023-08-30 18:49:34 +0400

    stage 4 completed

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 import datetime
-
-from django.db import models
-from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
